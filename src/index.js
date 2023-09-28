914b253343b3579de1ff7553ffd81ee72dea589a
ludiaz978 <142175671+ludiaz978@users.noreply.github.com>
2023-09-27 17:29:14 -0700

    working on it

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-
+import { StrictMode } from "react";
+import {createRoot} from"react-dom/client";
 import App from './App';
+import 'bootstrap/dist/css/bootstrap.min.css';
+const container = document.getElementById("root");
 
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = createRoot (container);
 root.render(
-  <React.StrictMode>
+  <StrictMode>
     <App />
-  </React.StrictMode>
+  </StrictMode>
 );
 
 
