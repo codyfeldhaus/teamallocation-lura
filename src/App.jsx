7ae85813ce2eaebbfc4bd3851b579a27c371b820
ludiaz978 <142175671+ludiaz978@users.noreply.github.com>
2023-09-28 17:48:54 -0700

    saved

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
 import Header from './Header';
-import Employees from './;Employees';
+import Employees from './Employees';
 import Footer from './Footer';
 
 function App () {
--- src/Header.jsx
+++ src/Header.jsx
@@ -2,9 +2,9 @@ const Header = () => {
 
     return (
     <header>
-            <h1 className="mt-4 p5 bg -primary text-white rounded">Header</h1>
+            <h1 className="mt-4 p5 bg -primary text-black rounded">Header</h1>
     </header>
     )
     
-    }
-    export default Header
+}
+export default Header
